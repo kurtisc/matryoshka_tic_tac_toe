8f304f6a1a87df65f6439791470509147164c757
Kurtis Charnock <kurtis@charno.cc>
2021-05-31 01:31:31 +0100

    Remove progress resume
    This is no longer any faster
    
    Signed-off-by: Kurtis Charnock <kurtis@charno.cc>

--- src/lookup-generator/main.rs
+++ src/lookup-generator/main.rs
@@ -2,11 +2,16 @@ extern crate getopts;
 use getopts::Options;
 use matryoshka_tic_tac_toe::game::Game;
 use matryoshka_tic_tac_toe::solver::Solver;
-use serde::{Deserialize, Serialize};
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::time::SystemTime;
+
+#[derive(Debug, Clone)]
+struct Progress {
+    number_of_pieces: usize,
+    row: usize,
+    col: usize,
+    piece: usize,
+}
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} FILE [options]", program);
@@ -15,8 +20,12 @@ fn print_usage(program: &str, opts: Options) {
 
 fn main() {
     let solver = Solver::new_overwrite_lookup();
-    let mut progress = Progress::new();
-    let mut resumed = false;
+    let mut progress = Progress {
+        number_of_pieces: 0,
+        row: 0,
+        col: 0,
+        piece: 0,
+    };
     let mut until: usize = 7;
 
     let args: Vec<String> = env::args().collect();
@@ -46,31 +55,22 @@ fn main() {
         _ => (),
     };
 
-    for mut number_of_pieces in 3..until + 1 {
-        for mut row in 0..3 {
-            for mut col in 0..3 {
-                for mut piece in 0..number_of_pieces {
-                    if !resumed {
-                        number_of_pieces = progress.number_of_pieces;
-                        row = progress.row;
-                        col = progress.col;
-                        piece = progress.piece;
-                        resumed = true;
-                    }
-
+    for number_of_pieces in 3..until + 1 {
+        for row in 0..3 {
+            for col in 0..3 {
+                for piece in 0..number_of_pieces {
                     progress.number_of_pieces = number_of_pieces;
                     progress.row = row;
                     progress.col = col;
                     progress.piece = piece;
-                    progress.write();
                     println! {"{:?}", progress};
 
                     let now = SystemTime::now();
                     let mut game = Game::new_with_size(number_of_pieces);
                     game = game.make_move(row, col, piece).unwrap();
                     let (i, j, k) = solver.find_move(&game);
-                    let game = game.make_move(i, j, k).unwrap();
-                    do_next_lookup(&game, &solver, &number_of_pieces);
+                    let mut game = game.make_move(i, j, k).unwrap();
+                    do_next_lookup(&mut game, &solver, &number_of_pieces);
 
                     println! {"{}", now.elapsed().unwrap().as_secs()};
                 }
@@ -79,76 +79,35 @@ fn main() {
     }
 }
 
-fn do_next_lookup(game: &Game, solver: &Solver, number_of_pieces: &usize) {
-    let mut game = game.clone();
-    while !game.is_finished() {
-        for row in 0..3 {
-            for col in 0..3 {
-                for piece in 0..*number_of_pieces {
-                    let new_game = game.clone().make_move(row, col, piece);
-                    match new_game {
-                        Ok(_) => {
-                            game = game.make_move(row, col, piece).unwrap();
-                            if game.is_finished() {
-                                return;
-                            }
-                            let (i, j, k) = solver.find_move(&game);
-                            let new_game = game.clone().make_move(i, j, k);
-                            match new_game {
-                                Ok(_) => {
-                                    game = game.clone().make_move(i, j, k).unwrap();
-                                    if game.is_finished() {
-                                        return;
-                                    }
-                                }
-                                _ => (),
-                            }
+fn do_next_lookup(game: &mut Game, solver: &Solver, number_of_pieces: &usize) {
+    for row in 0..3 {
+        for col in 0..3 {
+            for piece in 0..*number_of_pieces {
+                if game.get_turn_count() > 4 {
+                    return;
+                }
+                let new_game = game.clone().make_move(row, col, piece);
+                match new_game {
+                    Ok(_) => {
+                        let mut game = new_game.unwrap();
+                        if game.is_finished() {
+                            return;
                         }
-                        _ => (),
+                        let (i, j, k) = solver.find_move(&game);
+                        let new_game = game.clone().make_move(i, j, k);
+                        match new_game {
+                            Ok(_) => {
+                                game = game.make_move(i, j, k).unwrap();
+                                if game.is_finished() {
+                                    return;
+                                }
+                            }
+                            _ => (),
+                        }
                     }
+                    _ => (),
                 }
             }
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Progress {
-    number_of_pieces: usize,
-    row: usize,
-    col: usize,
-    piece: usize,
-}
-
-impl Progress {
-    fn write(self: &Self) {
-        let mut f = File::create("data/generator-progress.json").unwrap();
-        f.write_all(&serde_json::to_string(self).unwrap().into_bytes())
-            .unwrap();
-        f.sync_all().unwrap();
-    }
-
-    fn new() -> Self {
-        let f = File::open("data/generator-progress.json");
-        match f {
-            Ok(mut file) => {
-                let mut contents = String::new();
-                match file.read_to_string(&mut contents) {
-                    Ok(_) => serde_json::from_str(&contents.to_string()).unwrap(),
-                    _ => Progress {
-                        number_of_pieces: 3,
-                        row: 0,
-                        col: 0,
-                        piece: 0,
-                    },
-                }
-            }
-            _ => Progress {
-                number_of_pieces: 3,
-                row: 0,
-                col: 0,
-                piece: 0,
-            },
         }
     }
 }
